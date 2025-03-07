8fbd4495085833b39ccb3ea5a9b9884b35cebecc
ryan <rom@ualberta.ca>
2025-03-06 22:13:20 -0700

    change logging

--- create_iptables.sh
+++ create_iptables.sh
@@ -16,6 +16,11 @@ iptables -Z
 iptables -t nat -F
 iptables -t mangle -F
 
+
+iptables -N LOG_AND_DROP
+iptables -A LOG_AND_DROP -j LOG --log-prefix "Dropped: "
+iptables -A LOG_AND_DROP -j DROP
+
 # Default Policies
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
@@ -38,21 +43,17 @@ iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
 iptables -A FORWARD -p icmp --icmp-type echo-request -j ACCEPT
 
 # Allow FTP/TFTP to Windows, but block 10.229.10.0/24
-iptables -A FORWARD -s 10.229.10.0/24 -d 10.229.1.2 -p tcp --dport 20:21 -m limit --limit 20/min -j LOG --log-prefix "DROP: FTP to Windows - " --log-level 7
-iptables -A FORWARD -s 10.229.10.0/24 -d 10.229.1.2 -p tcp --dport 20:21 -j DROP
+iptables -A FORWARD -s 10.229.10.0/24 -d 10.229.1.2 -p tcp --dport 20:21 -j LOG_AND_DROP
 
-iptables -A FORWARD -s 10.229.10.0/24 -d 10.229.1.2 -p udp --dport 69 -m limit --limit 20/min -j LOG --log-prefix "DROP: TFTP to Windows - " --log-level 7
-iptables -A FORWARD -s 10.229.10.0/24 -d 10.229.1.2 -p udp --dport 69 -j DROP
+iptables -A FORWARD -s 10.229.10.0/24 -d 10.229.1.2 -p udp --dport 69 -j LOG_AND_DROP
 
 iptables -A FORWARD -d 10.229.1.2 -p tcp --dport 20:21 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -d 10.229.1.2 -p udp --dport 69 -j ACCEPT
 
 # Allow FTP/TFTP to Linux, but block 10.229.11.0/24
-iptables -A INPUT -s 10.229.11.0/24 -p tcp --dport 20:21 -m limit --limit 20/min -j LOG --log-prefix "DROP: FTP to Linux - " --log-level 7
-iptables -A INPUT -s 10.229.11.0/24 -p tcp --dport 20:21 -j DROP
+iptables -A INPUT -s 10.229.11.0/24 -p tcp --dport 20:21 -j LOG_AND_DROP
 
-iptables -A INPUT -s 10.229.11.0/24 -p udp --dport 69 -m limit --limit 20/min -j LOG --log-prefix "DROP: TFTP to Linux - " --log-level 7
-iptables -A INPUT -s 10.229.11.0/24 -p udp --dport 69 -j DROP
+iptables -A INPUT -s 10.229.11.0/24 -p udp --dport 69 -j LOG_AND_DROP
 
 iptables -A INPUT -d 10.229.1.1 -p tcp --dport 20:21 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A INPUT -d 10.229.1.1 -p udp --dport 69 -j ACCEPT
@@ -62,12 +63,6 @@ iptables -A INPUT -p tcp --dport 1024:65535 -m state --state ESTABLISHED -j ACCE
 iptables -A OUTPUT -p tcp --sport 1024:65535 -m state --state NEW,ESTABLISHED -j ACCEPT
 
 # Block Windows from accessing 10.229.10.0/24 (outbound)
-iptables -A OUTPUT -s 10.229.1.2 -d 10.229.10.0/24 -m limit --limit 20/min -j LOG --log-prefix "DROP: Windows Outbound - " --log-level 7
-iptables -A OUTPUT -s 10.229.1.2 -d 10.229.10.0/24 -j DROP
-
-# Log all other dropped traffic
-iptables -A INPUT -m limit --limit 20/min -j LOG --log-prefix "DROP: INPUT - " --log-level 7
-iptables -A FORWARD -m limit --limit 20/min -j LOG --log-prefix "DROP: FORWARD - " --log-level 7
-iptables -A OUTPUT -m limit --limit 20/min -j LOG --log-prefix "DROP: OUTPUT - " --log-level 7
+iptables -A OUTPUT -s 10.229.1.2 -d 10.229.10.0/24 -j LOG_AND_DROP
 
 echo "Firewall rules applied successfully!"
